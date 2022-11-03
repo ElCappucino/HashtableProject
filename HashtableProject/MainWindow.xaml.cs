bb1a305687baa27f425f26a0fe7a83d06b6ecae5
RodITimMaa <boonglostsaga@gmail.com>
2022-11-03 22:46:14 +0700

    Initial commit

---
+++ HashtableProject/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HashtableProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hashtable hashtable;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Add
+            hashtable.Add(Keytxt.Text, Valuetxt.Text);
+        }
+
+        private void RemoveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Remove
+            hashtable.Remove(Keytxt.Text);
+        }
+
+        private void ShowAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection icollection = hashtable.Keys;
+            foreach (string key in icollection)
+            {
+                MessageBox.Show(hashtable[key].ToString());
+            }
+        }
+    }
+}
